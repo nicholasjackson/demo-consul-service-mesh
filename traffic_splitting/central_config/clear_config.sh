3b7b1a21840a121ef00b8149c7da026903bd11d9
Nic Jackson <jackson.nic@gmail.com>
2019-09-11 13:48:15 -0700

    Updated demos

--- traffic_splitting/central_config/clear_config.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-consul config delete -kind service-splitter -name payments
-consul config delete -kind service-resolver -name payments
-consul config delete -kind service-splitter -name payments-split
-consul config delete -kind service-resolver -name payments-split
